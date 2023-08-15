00b19cc4052dedb166702f7358c1a20ae3fead93
Zaure <Zsaigutina@gmail.com>
2023-08-15 13:44:36 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void returnSendData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет"));
+    }
+
+}
