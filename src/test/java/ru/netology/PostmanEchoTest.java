cb3414bfb2dd478f574ea975e19b54f6101a71ff
Zaure <Zsaigutina@gmail.com>
2023-08-15 14:09:12 +0300

    Correct test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет"));
+                .body("data", equalTo("Hi"));
     }
 
 }
